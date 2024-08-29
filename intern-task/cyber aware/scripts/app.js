ebd4c00ee3326300ed15d6d461fdeade4ad4e6a7
saroj adhikari <adhikarisaroj291@gmail.com>
2024-08-29 17:31:45 +0545

    mobile responsive

--- intern-task/cyber aware/scripts/app.js
+++ intern-task/cyber aware/scripts/app.js
@@ -3,3 +3,19 @@ document.querySelector(".menu-toggle").addEventListener("click", () => {
   document.querySelector(".menu-toggle").classList.toggle("change");
   document.querySelector(".overlay").classList.toggle("active");
 });
+
+document.querySelector(".aside-arrow").addEventListener("click", () => {
+  document
+    .querySelector(".main-content-left")
+    .classList.toggle("active-sidebar");
+
+  document
+    .querySelector(".main-content-right")
+    .classList.toggle("active-sidebar");
+
+  const asideSidelinks = document.querySelectorAll(".aside-navlink");
+
+  asideSidelinks.forEach((asidelink) => {
+    asidelink.classList.toggle("aside-a-block");
+  });
+});
